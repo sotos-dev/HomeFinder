0734d0c57cf077b597e6155e04df9b8fa73bc2dd
SotoS <xsavannahx@protonmail.com>
2022-06-01 17:23:01 +0300

    Homepage | search-form

--- components/homepageComps/heroSection/HeroFormMobile.jsx
+++ components/homepageComps/heroSection/HeroFormMobile.jsx
@@ -3,8 +3,21 @@ import HeroImage from "../../../assets/images/hero-image.jpg"
 import { MdKeyboardArrowDown } from "react-icons/md"
 import { bedrooms, totalMaxPrice } from "../../../utils/homepageMobileData"
 import Option from "./Option"
+import { useState } from "react"
 
 const HeroFormMobile = () => {
+  const [forSaleButton, setForSaleButton] = useState(true)
+  const [toRentButton, setToRentButton] = useState(false)
+
+  const saleButton = () => {
+    setForSaleButton((prev) => (prev = !prev))
+    setToRentButton((prev) => (prev = !prev))
+  }
+  const rentButton = () => {
+    setToRentButton((prev) => (prev = !prev))
+    setForSaleButton((prev) => (prev = !prev))
+  }
+
   return (
     <>
       <div>
@@ -19,6 +32,29 @@ const HeroFormMobile = () => {
           <span className='font-medium'>UK</span>
         </p>
         <form className='mt-5'>
+          {/* For Sale/Rent */}
+          <div className='flex justify-between mb-4'>
+            <button
+              onClick={saleButton}
+              type='button'
+              className={`${
+                forSaleButton
+                  ? "bg-myBlue text-white"
+                  : "bg-slate-100 text-black"
+              } text-white py-5 px-10  w-1/2 rounded-md`}>
+              For sale
+            </button>
+            <button
+              onClick={rentButton}
+              type='button'
+              className={`${
+                toRentButton
+                  ? "bg-myBlue text-white"
+                  : "bg-slate-100 text-black"
+              } py-5 px-10 w-1/2 rounded-md`}>
+              To rent
+            </button>
+          </div>
           {/* Search area */}
           <fieldset className='relative'>
             <p className='absolute top-2 left-4 text-sm text-myBlue'>
@@ -27,6 +63,7 @@ const HeroFormMobile = () => {
             <input
               type='text'
               placeholder='eg. Oxford or NW3'
+              autoComplete='off'
               id='search-area'
               className='border border-opacity-30 border-myBlue bg-white w-full h-16 rounded-md pl-4 pt-6 text-lg'
             />
@@ -35,25 +72,29 @@ const HeroFormMobile = () => {
           <div className='flex justify-between items-center gap-4 mt-5'>
             {/* Bedrooms */}
             <fieldset className='relative flex-grow'>
-              <p className='absolute top-2 left-4 text-sm text-myBlue'>
+              <p className='pointer-events-none absolute top-2 left-4 text-sm text-myBlue'>
                 Bedrooms
               </p>
-              {/* <MdKeyboardArrowDown className='absolute text-3xl bottom-1 right-4' /> */}
-              <select className='border border-opacity-30 border-myBlue bg-white w-full h-16 rounded-md pl-4 pt-6 text-lg '>
+              <MdKeyboardArrowDown className='pointer-events-none absolute text-3xl bottom-1 right-4' />
+              <select
+                id='bedrooms'
+                className='border border-opacity-30 border-myBlue bg-white w-full h-16 rounded-md pl-4 pt-6 text-lg appearance-none'>
                 {bedrooms.map((room) => (
-                  <Option optionName={room} />
+                  <Option key={room + 1} optionName={room} />
                 ))}
               </select>
             </fieldset>
             {/* Max price */}
             <fieldset className='relative flex-grow'>
-              <p className='absolute top-2 left-4 text-sm text-myBlue'>
+              <p className='pointer-events-none absolute top-2 left-4 text-sm text-myBlue'>
                 Max price
               </p>
-              {/* <MdKeyboardArrowDown className='absolute text-3xl bottom-1 right-4' /> */}
-              <select className='border border-opacity-30 border-myBlue bg-white w-full h-16 rounded-md pl-4 pt-6 text-lg '>
+              <MdKeyboardArrowDown className='pointer-events-none absolute text-3xl bottom-1 right-4' />
+              <select
+                selected
+                className='border border-opacity-30 border-myBlue bg-white w-full h-16 rounded-md pl-4 pt-6 text-lg appearance-none'>
                 {totalMaxPrice.map((price) => {
-                  return <Option optionName={price} />
+                  return <Option key={price + 1} optionName={price} />
                 })}
               </select>
             </fieldset>
